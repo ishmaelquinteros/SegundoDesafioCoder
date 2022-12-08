ccb3e9ad650f6651546d6531776c3335421e2f6d
Ismael Quinteros <ishmaelquinteros@gmail.com>
2022-12-07 21:02:06 -0300

    Incorporando angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { ContenidoComponent } from './componentes/contenido/contenido.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialComponent } from './componentes/material/material.component';
+import { MaterialModule } from './modulos/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    ContenidoComponent
+    ContenidoComponent,
+    MaterialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
